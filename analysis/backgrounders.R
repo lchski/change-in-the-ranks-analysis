0453f76250b60fff8438306486b934ee0d8b9216
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-05-02 17:07:07 -0400

    approach problem from another angle to wrangle problematic pages

--- analysis/backgrounders.R
+++ analysis/backgrounders.R
@@ -1,28 +1,50 @@
-
-backgrounder_paragraphs_raw
-
-backgrounders2 <- backgrounders %>%
-  mutate(content = map_chr(page, ~ .x %>% html_node("article.full-article .content-news-article") %>% as.character))
-
+library(textclean)
 
 backgrounders %>%
+  filter(id %in% broken_backgrounder_ids) %>%
+  slice(1) %>%
+  pull(page) %>%
+  map(~ .x %>%
+        html_node("article.full-article") %>%
+        html_node(".content-news-article") %>%
+        as.character %>%
+        replace_html)
+
+backgrounders2 <- backgrounders %>%
+  filter(id %in% broken_backgrounder_ids) %>%
+  mutate(text = map_chr(page, ~ .x %>%
+        html_node("article.full-article") %>%
+        html_node(".content-news-article") %>%
+        as.character %>%
+        replace_html))
+
+backgrounders2 %>%
+  select(id, date, title, text) %>%
+  unnest_tokens(token, text, token = "regex", pattern = "\n")
+
+
+backgrounder_paragraphs <- backgrounders %>%
   select(id, date, title, text) %>%
   unnest_tokens(token, text, token = "regex", pattern = "\n") %>%
   mutate(token = trimws(token)) %>%
-  mutate(educ = str_detect(token, "^education")) %>%
-  filter(educ) %>%
-  mutate(token = map(token, function(line_to_parse) {
-    if (str_detect(line_to_parse, "^education$")) {
-      return(list(line_to_parse))
-    }
+  filter(token != "") %>%
+  mutate(section = case_when(
+    str_detect(token, "^education") ~ "education",
+    str_detect(token, "^professional experience") ~ "professional experience",
+    str_detect(token, "^related products") ~ "related products",
+    TRUE ~ NA_character_
+  ))
 
-    if (str_detect(line_to_parse, "^education")) {
-      return(c("education", str_split(line_to_parse, "^education")))
-    }
+## find the backgrounders which, for whatever reason, don't properly parse into multiple lines
+broken_backgrounder_ids <- backgrounder_paragraphs %>%
+  count_group(id) %>%
+  filter(count == 1) %>%
+  pull(id)
 
-    return(list(line_to_parse))
-  })) %>%
-  unnest(c(token)) %>%
-  unnest(c(token)) %>%
-  mutate(token = trimws(token)) %>%
-  filter(token != "")
+backgrounders %>%
+  filter(id %in% broken_backgrounder_ids) %>%
+  slice(1) %>%
+  pull(page) %>%
+  map(~ .x %>%
+        html_node("article.full-article"))
+  
