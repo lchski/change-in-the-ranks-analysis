dd31e5c4c1b8b9ce285a1b27b6b0ed45a3be8154
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-05-02 08:31:50 -0400

    add downloader feature to scraper

--- load/download-announcement-pages.R
+++ load/download-announcement-pages.R
@@ -8,16 +8,48 @@ test_page <- read_html(test_url)
 
 
 ## Check if URL in storage, download and save if not.
-retrieve_page_at_url <- function(url, redownload = FALSE) {
+retrieve_page_at_url <- function(url, scrape_waiting_period = 5) {
+  file_to_return <- NULL
+
   file_path <- url %>% convert_url_to_filename
+
+  file_is_already_downloaded <- file_exists(file_path)
   
-  file_already_downloaded <- file_exists(file_path)
-  
-  if (file_already_downloaded) {
-    return(read_html(file_path))
+  if (file_is_already_downloaded) {
+   file_to_return = read_html(file_path)
+  } else {
+    tryCatch(
+      {
+        Sys.sleep(scrape_waiting_period)
+
+        file_to_return = read_html(url)
+
+        file_to_return %>% write_html(file_path)
+      },
+      error = function(c) {
+        message(
+          paste0(
+            "Got an error when trying to read_html a page\n\t ",
+            "url = ", url,
+            "error = ", c
+          )
+        )
+      }
+    )
   }
-  
-  ## TODO: the rest... also `redownload`
+
+  if (is_null(file_to_return)) {
+    message(
+      paste0(
+        "Could not retrieve a page\n\t ",
+        "url = ", url
+      )
+    )
+
+    return(list())
+  }
+
+  return(file_to_return)
 }
 
 convert_url_to_filename <- function(url) {
@@ -68,3 +100,5 @@ process_announcement_page <- function(page_to_process) {
 }
 
 process_announcement_page(test_page)
+
+
