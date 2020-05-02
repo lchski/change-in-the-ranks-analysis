4eae9795179fb50f919ab38dcae58039cab3eace
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-05-01 20:03:34 -0400

    start sketching out the downloading code

--- load/download-announcement-pages.R
+++ load/download-announcement-pages.R
@@ -1,9 +1,39 @@
 library(rvest)
+library(fs)
 
 test_url <- urls %>% slice(1) %>% pull(original_url)
 
 test_page <- read_html(test_url)
 
+
+
+## Check if URL in storage, download and save if not.
+retrieve_page_at_url <- function(url, redownload = FALSE) {
+  file_path <- url %>% convert_url_to_filename
+  
+  file_already_downloaded <- file_exists(file_path)
+  
+  if (file_already_downloaded) {
+    return(read_html(file_path))
+  }
+  
+  ## TODO: the rest... also `redownload`
+}
+
+convert_url_to_filename <- function(url) {
+  url %>%
+    str_replace_all(fixed("/"), fixed("SLASH")) %>%
+    str_replace_all(fixed(":"), fixed("COLON")) %>%
+    path("data/source/downloaded-pages", ., ext = "html")
+}
+
+convert_filename_to_url <- function(filename) {
+  filename %>%
+    str_remove(fixed("data/source/downloaded-pages/")) %>%
+    str_remove(fixed(".html")) %>%
+    str_replace_all(fixed("SLASH"), fixed("/")) %>%
+    str_replace_all(fixed("COLON"), fixed(":"))
+}
 
 
 process_announcement_page <- function(page_to_process) {
