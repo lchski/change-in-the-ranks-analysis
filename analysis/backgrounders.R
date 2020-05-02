abb2bcf6704974b291a29e9de5fbf42c1582655e
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-05-02 17:16:05 -0400

    successfully extract lines from all backgrounders

--- analysis/backgrounders.R
+++ analysis/backgrounders.R
@@ -24,6 +24,14 @@ backgrounders2 %>%
 
 
 backgrounder_paragraphs <- backgrounders %>%
+  mutate(text = map_chr(
+    page,
+    ~ .x %>%
+      html_node("article.full-article") %>%
+      html_node(".content-news-article") %>%
+      as.character %>%
+      replace_html
+  )) %>%
   select(id, date, title, text) %>%
   unnest_tokens(token, text, token = "regex", pattern = "\n") %>%
   mutate(token = trimws(token)) %>%
@@ -31,7 +39,7 @@ backgrounder_paragraphs <- backgrounders %>%
   mutate(section = case_when(
     str_detect(token, "^education") ~ "education",
     str_detect(token, "^professional experience") ~ "professional experience",
-    str_detect(token, "^related products") ~ "related products",
+    str_detect(token, "^related product") ~ "related products",
     TRUE ~ NA_character_
   ))
 
