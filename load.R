0d0f66707601f6e575ac4f7b4457310712d14db5
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-05-02 10:54:32 -0400

    download backgrounders

--- load.R
+++ load.R
@@ -2,21 +2,40 @@ library(tidyverse)
 library(jsonlite)
 library(lubridate)
 
+library(helpers)
+
 source("load/download-announcement-pages.R")
 
 urls_raw <- tibble(web_archive_url = read_json("data/source/urls.json", simplifyVector = TRUE))
 
 announcements <- urls_raw %>%
-  mutate(original_url = str_remove(web_archive_url, fixed("http://web.archive.org/web/*/"))) %>%
-  mutate(date = as_date(str_extract(original_url, "[0-9]{4}/[0-9]{2}/[0-9]{2}"))) %>%
+  mutate(url = str_remove(web_archive_url, fixed("http://web.archive.org/web/*/"))) %>%
+  mutate(date = as_date(str_extract(url, "[0-9]{4}/[0-9]{2}/[0-9]{2}"))) %>%
   arrange(date) %>%
-  mutate(page = map(original_url, retrieve_page_at_url)) %>%
+  mutate(page = map(url, retrieve_page_at_url)) %>%
   mutate(is_actual_result = ! map_lgl(page, is_bare_list)) %>%
   filter(is_actual_result) %>%
   select(-is_actual_result, -web_archive_url) %>%
   mutate(id = row_number()) %>%
-  select(id, everything())
-
-announcements %>%
+  select(id, everything()) %>%
   mutate(announcement = map(page, process_announcement_page)) %>%
   unnest_wider(c(announcement))
+
+backgrounder_links <- announcements %>%
+  select(id, backgrounder_links) %>%
+  unnest(c(backgrounder_links)) %>%
+  unnest(c(backgrounder_links))
+
+backgrounder_link_urls <- backgrounder_links %>%
+  select(urls) %>%
+  distinct() %>%
+  filter(! is.na(urls)) %>%
+  mutate(url = paste0("https://pm.gc.ca", urls)) %>%
+  select(-urls) %>%
+  mutate(url = str_replace(url, fixed("https://pm.gc.cahttps://pm.gc.ca/"), fixed("https://pm.gc.ca/")))
+
+backgrounders <- backgrounder_link_urls %>%
+  mutate(date = as_date(str_extract(url, "[0-9]{4}/[0-9]{2}/[0-9]{2}"))) %>%
+  arrange(date) %>%
+  mutate(page = map(url, retrieve_page_at_url))
+
