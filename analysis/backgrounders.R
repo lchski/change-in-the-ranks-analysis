b08e8adf09e73b1785acb45f7c7faa341d1ca5b5
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-05-02 17:16:29 -0400

    remove debug code

--- analysis/backgrounders.R
+++ analysis/backgrounders.R
@@ -1,28 +1,5 @@
 library(textclean)
 
-backgrounders %>%
-  filter(id %in% broken_backgrounder_ids) %>%
-  slice(1) %>%
-  pull(page) %>%
-  map(~ .x %>%
-        html_node("article.full-article") %>%
-        html_node(".content-news-article") %>%
-        as.character %>%
-        replace_html)
-
-backgrounders2 <- backgrounders %>%
-  filter(id %in% broken_backgrounder_ids) %>%
-  mutate(text = map_chr(page, ~ .x %>%
-        html_node("article.full-article") %>%
-        html_node(".content-news-article") %>%
-        as.character %>%
-        replace_html))
-
-backgrounders2 %>%
-  select(id, date, title, text) %>%
-  unnest_tokens(token, text, token = "regex", pattern = "\n")
-
-
 backgrounder_paragraphs <- backgrounders %>%
   mutate(text = map_chr(
     page,
@@ -42,17 +19,3 @@ backgrounder_paragraphs <- backgrounders %>%
     str_detect(token, "^related product") ~ "related products",
     TRUE ~ NA_character_
   ))
-
-## find the backgrounders which, for whatever reason, don't properly parse into multiple lines
-broken_backgrounder_ids <- backgrounder_paragraphs %>%
-  count_group(id) %>%
-  filter(count == 1) %>%
-  pull(id)
-
-backgrounders %>%
-  filter(id %in% broken_backgrounder_ids) %>%
-  slice(1) %>%
-  pull(page) %>%
-  map(~ .x %>%
-        html_node("article.full-article"))
-  
