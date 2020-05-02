4e1171f359ea306fc02b74066bb1aa12687048d2
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-05-02 11:20:16 -0400

    remove debug string

--- analysis/announcements.R
+++ analysis/announcements.R
@@ -2,10 +2,6 @@
 announcements %>%
   select(id, date, text) %>%
   unnest_tokens(token, text, token = "sentences") %>%
-  mutate(token = str_remove_all(token, "the prime minister, justin trudeau, today announced the following changes{,1} in the senior ranks of the public service:")) %>%
+  mutate(token = str_remove_all(token, "the prime minister, justin trudeau, today announced the following changes{0,1} in the senior ranks of the public service:")) %>%
   mutate(token = trimws(token)) %>%
   filter(token != "")
-
-
-
-the prime minister, justin trudeau, today announced the following changes in the senior ranks of the public service: malcolm brown, currently special advisor to the clerk of the privy council on the syrian refugee initiative, becomes deputy minister of public safety, effective april 4, 2016.
