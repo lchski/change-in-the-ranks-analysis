fdef2765a06938ba3122360c9b2a7743ca9b7561
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-03-05 16:37:29 -0500

    Initial commit of scraper

---
+++ scripts/update-announcement-urls.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+
+
+/*
+
+1. Load existing URLs from disk.
+2. Start on page 0 of pm.gc.ca news releases. If there’s a 404, quit and save the URLs. Pull URLs, filtering for “change in the ranks”.
+3. Compare URLs scraped to URLs from disk. If there’s a match, go to last step.
+4. If all the URLs scraped are new, go to step 2, using page n+1 of pm.gc.ca news releases.
+5. Save expanded list of URLs to disk.
+
+*/
+
+async function fetchNewsReleaseHtmlJSON(page) {
+    const response = await fetch("https://pm.gc.ca/views/ajax", {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            // "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"",
+            // "sec-ch-ua-mobile": "?0",
+            // "sec-ch-ua-platform": "\"macOS\"",
+            // "sec-fetch-dest": "empty",
+            // "sec-fetch-mode": "cors",
+            // "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+            // "Referer": "https://pm.gc.ca/en/news/releases?page=1",
+            // "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": `view_name=news&view_display_id=page_1&view_args=1&page=${page}`,
+        "method": "POST"
+    });
+    const responseJSON = await response.json();
+
+    return responseJSON;
+}
+
+let results = await fetchNewsReleaseHtmlJSON(81);
+
+console.log(results);
+
+console.log(results.filter((contentItem) => {
+    return contentItem.command == "insert" && contentItem.selector == ".js-view-dom-id-";
+}).pop()['data']);
